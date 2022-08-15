7af1d71cc18d4dfc51f45c3003d344e101303109
oseijoel6111 <oseijoel6111@gmail.com>
2022-08-15 04:27:00 -0700

    Finished V1.0

---
+++ colors.py
@@ -0,0 +1,6 @@
+# Defining Some Colors
+BLUE = '\033[0;34m'
+GREEN = '\033[0;32m'
+RED = '\033[0;31m'
+WHITE = '\033[0;37m'
+YELLOW = '\033[0;33m'
---
+++ main.py
@@ -0,0 +1,53 @@
+from colors import *
+from random import randint
+
+# Welcome Screen Msg
+print(BLUE, '**** Welcome To Your World Of Guessing *****')
+user_input = int(input(WHITE + 'Choose Your Level\n'
+                               '1. Normal (1 - 50)\n'
+                               '2. Intermediate (1 - 200)\n'
+                               '3. Hard (1 - 500)\n'
+                               '4. Exit Application\n\n'
+                               'Enter Request : '))
+
+# user's option
+guess_range = 0
+if user_input == 1:
+    guess_range = 50
+elif user_input == 2:
+    guess_range = 200
+elif user_input == 3:
+    guess_range = 500
+elif user_input == 4:
+    print('***** Thanks For Using Our App, Hope To See You Again **** ')
+    exit(1)
+else:
+    print(RED, 'Invalid Request, Try Again.')
+
+#  Generate random number between certain range
+generate_number = randint(1, guess_range)
+
+# user chances
+user_chances = 4
+
+# accepting the user input and validating, if it matches the generated number
+for i in range(4):
+    user_guess = int(input(WHITE + 'Provide Your Guess Number : '))
+
+    # Determine if the guess is correct
+    if user_guess == generate_number:
+        print(GREEN, f'Hurray!!!!, You Guess It Correct')
+        break
+    elif user_guess > generate_number:
+        print(YELLOW, 'Your Guess Too High, Try Again.')
+    elif user_guess < generate_number:
+        print(YELLOW, 'Your Guess Too Low, Try Again')
+
+    #  reduce the user chances and prompt user chances left
+    user_chances -= 1
+    print(f'Number Of Chances Left is {user_chances}')
+
+
+# inform user of the correct guess number
+if user_chances == 0:
+    print(RED, f'Sorry The Correct Guess is {generate_number}')
